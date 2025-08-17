69bee3f36f7e93b972079edba8eca1efeb527c50
iEriic <120765275+iEriic@users.noreply.github.com>
2025-08-17 03:38:35 -0300

    Update sendmail.js

--- api/sendmail.js
+++ api/sendmail.js
@@ -1,34 +1,14 @@
-import nodemailer from "nodemailer";
-
-export default async function handler(req, res) {
-  if (req.method !== "POST") {
-    return res.status(405).json({ message: "Método inválido" });
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
   }
+});
 
-  const { to, subject, message } = req.body;
-  if (!to || !subject || !message) {
-    return res.status(400).json({ message: "Campos faltando" });
-  }
-
-  try {
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: "cuberoleplayoficial@gmail.com",       // Substitua pelo seu Gmail
-        pass: "gwrhkfiqddnytzqt"          // Substitua pela senha de app
-      }
-    });
-
-    await transporter.sendMail({
-      from: '"Servidor MTA" <cuberoleplayoficial@gmail.com>',
-      to,
-      subject,
-      html: message
-    });
-
-    res.status(200).json({ message: "E-mail enviado com sucesso!" });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: err.message });
-  }
-}
+await transporter.sendMail({
+  from: `"Servidor MTA" <${process.env.EMAIL_USER}>`,
+  to,
+  subject,
+  html: message
+});
